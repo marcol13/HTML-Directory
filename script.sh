17951fd36dcfb951e17fa9baf08a8b19ae84f3e6
Marcin <marcinkrueger@onet.eu>
2020-06-20 13:44:58 +0200

    creating index.html with all files

--- script.sh
+++ script.sh
@@ -4,15 +4,27 @@ list=()
 #ext=('d1=("jpg" "gif" "png" "jpeg" "tiff" "apng" "bmp" "ico" "cur" "jfif" "pjpeg" "pjp" "svg" "tif" "webp")' 'd2=("mp3" "ogg" "wav")' 'd3=("pdf" "doc" "odt")')
 #for i in "${ext[@]}";do eval $i;done
 
-
-#1-path;2-name;3-date;4-path_from_disk
+#1-path;2-name;3-date;4-hour;5-path_from_disk
 function create_element(){
     text="<div class=\"photo file-container\">
+                    <img src=\"\" alt=\"$1\"/>
+                    <p class=\"file-name\">$2</p>
+                    <p class=\"modify-date\">$3 $4</p>
+                    <p class=\"file-path\">$1</p>
+                    <a href=\"$5\"><img src=\"sid-view.png\" class=\"eye\"/></a>
+                </div>"
+    retval=$text
+    echo $retval
+}
+
+#1-path;2-name;3-date;4-hour;5-path_from_disk
+function create_img_element(){
+    text="<div class=\"photo photo-container\">
                     <img src=\"$1\" alt=\"$1\"/>
                     <p class=\"file-name\">$2</p>
-                    <p class=\"modify-date\">$3</p>
+                    <p class=\"modify-date\">$3 $4</p>
                     <p class=\"file-path\">$1</p>
-                    <a href=\"$4\"><img src=\"sid-view.png\" class=\"eye\"/></a>
+                    <a href=\"$5\"><img src=\"sid-view.png\" class=\"eye\"/></a>
                 </div>"
     retval=$text
     echo $retval
@@ -46,10 +58,52 @@ else
     name=( `awk '{print $1}' extension.txt` )
     description=( `awk '{print $2}' extension.txt` )
     ext=( `awk '{print $3}' extension.txt` )
+    code=""
     #sed -E "s/,/ /g"
     n=${#name[@]}
+    touch "index.html"
+    :>index.html
+    echo "<!DOCTYPE html>
+<html lang=\"pl\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <link rel=\"stylesheet\" href=\"style.css\">
+    <link href=\"https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap\" rel=\"stylesheet\"> 
+    <title>Moje pliki</title>
+</head>
+<body>
+    <div class=\"header\">
+        <h1 class=\"header-title\"><span class=\"blue\">Moje</span> <span class=\"yellow\">pliki</span></h1>
+        <div class=\"menu\">
+            <p class=\"menu-item\"><a href=\"#images\">Zdjęcia</a></p>
+            <p class=\"menu-item\"><a href=\"#music\">Muzyka</a></p>
+            <p class=\"menu-item\"><a href=\"#docs\">Dokumenty</a></p>
+        </div>
+    </div>
+    <div class=\"content\">" >> index.html
     for i in $(seq 0 $(($n-1)))
     do
+        code+="<section id=\"${name[$i]}\">
+                <h2 class=\"section-title\">${description[$i]}</h2>"
+        if [[ "${name[$i]}" == "images" ]]
+        then
+            code+="<div class=\"info-section photo-container\">
+                    <p class=\"info photo-size\">Obraz</p>
+                    <p class=\"info\">Nazwa pliku</p>
+                    <p class=\"info\">Data modyfikacji</p>
+                    <p class=\"info\">Ścieżka do pliku</p>
+                    <p class=\"info link-img\">Odnośnik</p>
+                </div>"
+        else
+            code+="<div class=\"info-section file-container\">
+                    <p class=\"info\">Plik</p>
+                    <p class=\"info\">Nazwa pliku</p>
+                    <p class=\"info\">Data modyfikacji</p>
+                    <p class=\"info\">Ścieżka do pliku</p>
+                    <p class=\"info link-img\">Odnośnik</p>
+                </div>"
+        fi
         temp_arr=(`echo ${ext[$i]} | sed -E "s/,/ /g" | sed -E "s/\"//g"`)
         #for j in ${list[@]}; do echo $j;done;
         temp_files=()
@@ -66,12 +120,20 @@ else
             time=`ls --full-time $j | grep -oE "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}"`
             path=`pwd`/$j
             file_name=`echo $j | cut -d '.' -f1 | rev | cut -d '/' -f1 | rev`
-            code=$(create_element $j $file_name $time $path)
+            if [[ "${name[$i]}" == "images" ]]
+            then
+                code+=$(create_img_element $j $file_name ${time[@]} $path)
+            else
+                code+=$(create_element $j $file_name ${time[@]} $path)
+            fi
         done
-        echo $i, ${temp_files[@]}
+        code+="</section>"
+        #echo $i, ${temp_files[@]}
     done
-
-    #echo ${ext[1]}
-    echo ${list[0]}
+    echo $code >> index.html
+    echo "</div>
+</body>
+</html>" >> index.html
 fi
 
+#echo $code
